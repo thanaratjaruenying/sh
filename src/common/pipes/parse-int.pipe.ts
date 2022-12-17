65f79d4d2b02507b3d622c8eee2157714f7ae8ae
aon <thanarat.develop@gmail.com>
2022-12-17 23:15:49 +0700

    feat(common): pipe

---
+++ src/common/pipes/parse-int.pipe.ts
@@ -0,0 +1,13 @@
+import { BadRequestException } from '@nestjs/common';
+import { PipeTransform, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ParseIntPipe implements PipeTransform<string> {
+  async transform(value: string) {
+    const val = parseInt(value, 10);
+    if (isNaN(val)) {
+      throw new BadRequestException('Validation failed');
+    }
+    return val;
+  }
+}

c05ea2743758d77317da9d22d556f6b9bdc51d80
aon <thanarat.develop@gmail.com>
2022-12-18 20:53:42 +0700

    refactor(user): add role on controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,19 +8,23 @@ import {
 } from '@nestjs/common';
 import { FastifyReply } from 'fastify';
 
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { SystemRole } from 'src/interfaces';
+import { JwtAuthGuard } from '../../common/jwt/jwt-auth.guard';
 import { GetUserDto } from './dto';
 import { UserService } from './user.service';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @Roles(SystemRole.SH, SystemRole.USER)
   @Get()
   async getUser(
     @Query() query: GetUserDto,
-    @Res() res: FastifyReply<any>,
+    @Res() res: FastifyReply,
   ): Promise<any> {
     const { email } = query;
 

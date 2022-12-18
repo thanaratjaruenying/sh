c2aa9a5437ac0ef26245db5583d6a92843d81d06
aon <thanarat.develop@gmail.com>
2022-12-18 10:53:12 +0700

    inject auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from './modules/config/config.module';
 import { RepositoryModule } from './modules/repositories/repository.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule, UserModule, RepositoryModule],
+  imports: [ConfigModule, UserModule, RepositoryModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

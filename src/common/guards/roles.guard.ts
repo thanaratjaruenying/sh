988a82e8eafdfb6e4b42183ac086dd22f5686f56
aon <thanarat.develop@gmail.com>
2022-12-18 17:44:07 +0700

    feat(guard): add role guard

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { SystemRole } from 'src/interfaces';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const hasRole = () =>
+      !!user.roles.find(
+        (role: SystemRole) => !!roles.find((item) => item === role),
+      );
+
+    return user && user.roles && hasRole();
+  }
+}
